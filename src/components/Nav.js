ac28d59eac8a955e28be418c6b753963b1d4eb80
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2023-10-15 21:22:16 -0400

    react app

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+function Nav(){
+    return (
+        <nav className='main-nav'>
+            <ul>
+                <li>Home</li>
+                <li>Global Temperatures</li>
+                <li>Electricity</li>
+                <li>Internet</li>
+                <li>Economic factors</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav; 
---
+++ src/components/Promo.js
@@ -0,0 +1,9 @@
+function Promo(){
+    return (
+        <div className='promo-section'>
+            <h1>DBMS PROJECT</h1>
+        </div>
+    );
+};
+
+export default Promo;
