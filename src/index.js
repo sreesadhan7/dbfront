1a1f7eb40e1775e30e71eae0e2647a15d7490018
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2023-10-26 16:21:15 -0400

    routes added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Home from "./views/Home.js";
 import Login from "./views/Login.js";
+import Graph from "./views/Graph.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -13,6 +14,7 @@ root.render(
     <Routes>
       <Route path="/" exact element={<Home />} />
       <Route path="/Login" exact element={<Login />} />
+      <Route path="/Graph" exact element={<Graph />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
---
+++ src/views/Graph.js
@@ -0,0 +1,7 @@
+function Graph() {
+        return (
+            <h1>TODO: Graph</h1>
+        )
+    }
+    
+    export default Graph;
