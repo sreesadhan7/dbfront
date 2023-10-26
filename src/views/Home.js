8c02adfdfd601d52e6e2316b62889d255206f354
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2023-10-26 17:22:28 -0400

    login page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,12 @@
+import Login from "../components/Login.js";
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
+
 function Home() {
         return (
-            <h1>TODO: HOME</h1>
+           <Login />
         )
     }
     
-    export default Home;
+export default Home;
