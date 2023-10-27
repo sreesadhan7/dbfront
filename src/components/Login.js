3e6a480382d324afc6beb9fc63a301f9c41d3c15
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2023-10-27 12:30:43 -0400

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -57,15 +57,13 @@ class Login extends React.Component {
             <Row className="row-grid justify-content-between align-items-center">
               <Col lg="6">
                 <h3 className="display-3 text-white">
-                  A beautiful Design System{" "}
-                  <span className="text-white">completed with examples</span>
+                  A Deep Dive Into Global Warming{" "}
+                  <span className="text-white">Trends and Factors</span>
                 </h3>
                 <p className="lead text-white">
-                  The Design System comes with four pre-built pages to help you
-                  get started faster. You can change the text and images and
-                  you're good to go. More importantly, looking at them will give
-                  you a picture of what you can built with this powerful
-                  Bootstrap 4 Design System.
+                  Project to display the trends and factors driving global warming.
+                  Electricity, temperatures, internet consumption, economic indices
+                  and how they relate to global warming.
                 </p>
                 <div className="btn-wrapper">
                   <Button color="success" to="/login-page" tag={Link}>
@@ -96,10 +94,10 @@ class Login extends React.Component {
                           onClick={(e) => e.preventDefault()}
                         >
                           <span className="btn-inner--icon mr-1">
-                            <img
-                              alt="..."
-                              src={require("../assets/img/github.svg")}
-                            />
+                          <a href="https://github.com" aria-label="Homepage" class="footer-octicon" title="GitHub">
+      <svg aria-hidden="true" class="octicon octicon-mark-github" height="24" version="1.1" viewBox="0 0 16 16" width="24"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+</svg>
+</a>
                           </span>
                           <span className="btn-inner--text">Github</span>
                         </Button>
@@ -110,10 +108,13 @@ class Login extends React.Component {
                           onClick={(e) => e.preventDefault()}
                         >
                           <span className="btn-inner--icon mr-1">
-                            <img
-                              alt="..."
-                              src={require("../assets/img/google.svg")}
-                            />
+                          <svg width="22.5%" height="24" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M29.6 20.2273C29.6 19.5182 29.5364 18.8364 29.4182 18.1818H20V22.05H25.3818C25.15 23.3 24.4455 24.3591 23.3864 25.0682V27.5773H26.6182C28.5091 25.8364 29.6 23.2727 29.6 20.2273Z" fill="#4285F4"/>
+<path d="M20 30C22.7 30 24.9636 29.1045 26.6181 27.5773L23.3863 25.0682C22.4909 25.6682 21.3454 26.0227 20 26.0227C17.3954 26.0227 15.1909 24.2636 14.4045 21.9H11.0636V24.4909C12.7091 27.7591 16.0909 30 20 30Z" fill="#34A853"/>
+<path d="M14.4045 21.9C14.2045 21.3 14.0909 20.6591 14.0909 20C14.0909 19.3409 14.2045 18.7 14.4045 18.1V15.5091H11.0636C10.3864 16.8591 10 18.3864 10 20C10 21.6136 10.3864 23.1409 11.0636 24.4909L14.4045 21.9Z" fill="#FBBC04"/>
+<path d="M20 13.9773C21.4681 13.9773 22.7863 14.4818 23.8227 15.4727L26.6909 12.6045C24.9591 10.9909 22.6954 10 20 10C16.0909 10 12.7091 12.2409 11.0636 15.5091L14.4045 18.1C15.1909 15.7364 17.3954 13.9773 20 13.9773Z" fill="#E94235"/>
+</svg>
+
                           </span>
                           <span className="btn-inner--text">Google</span>
                         </Button>
