b99869edc4352be9e0c34c4f44a981ccd3559fef
Tomas Delclaux Rodriguez-Rey <dlxtomas@gmail.com>
2023-11-20 14:38:22 -0500

    databse count added

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Energy from "./views/Energy.js";
 import GDP from "./views/GDP.js";
 import Internet from "./views/Internet.js";
 import Register from "./views/Register.js";
+import DatabaseCount from "./views/DatabaseCount.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -25,6 +26,7 @@ root.render(
       <Route path="/GDP" exact element={<GDP />} />
       <Route path="/Internet" exact element={<Internet />} />
       <Route path="/register" exact element={<Register />} />
+      <Route path="/DatabaseCount" exact element={<DatabaseCount />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
