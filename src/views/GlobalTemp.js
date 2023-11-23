15ec2b5bb6c9a6bbe7adca2ac416b2df891d4f1f
sreesadhan7 <sreesadhan7@gmail.com>
2023-11-23 12:43:41 -0500

    updated charts headings

--- src/views/GlobalTemp.js
+++ src/views/GlobalTemp.js
@@ -55,7 +55,7 @@ class GlobalTemp extends React.Component {
         },
         m_1_3: {
             country: "India",
-            from: "1960",
+            from: "1970",
             to: "2018",
             data: null
         }
@@ -114,7 +114,7 @@ class GlobalTemp extends React.Component {
     return options;
   }
   yearList11(param, url){
-    const yearList = [1960,1961,1962,1963,1964,1965,1966,1967,1968,1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022];
+    const yearList = [1960,1961,1962,1963,1964,1965,1966,1967,1968,1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019];
     const options = []
     for(let i=0; i<yearList.length; i++){
         options.push(<DropdownItem onClick={e=>this.changeYear(e,yearList[i], param, url)}><div>{yearList[i]}</div></DropdownItem>);
@@ -123,7 +123,7 @@ class GlobalTemp extends React.Component {
   }
 
   yearList12(param, url){
-    const yearList = [1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022];
+    const yearList = [1960,1961,1962,1963,1964,1965,1966,1967,1968,1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019];
     const options = []
     for(let i=0; i<yearList.length; i++){
         options.push(<DropdownItem onClick={e=>this.changeYear(e,yearList[i], param, url)}><div>{yearList[i]}</div></DropdownItem>);
@@ -132,32 +132,14 @@ class GlobalTemp extends React.Component {
   }
 
   yearList13(param, url){
-    const yearList = [1750, 1751, 1752, 1753, 1754, 1755, 1756, 1757, 1758, 1759, 1760, 1761,
-        1762, 1763, 1764, 1765, 1766, 1767, 1768, 1769, 1770, 1771, 1772,
-        1773, 1774, 1775, 1776, 1777, 1778, 1779, 1780, 1781, 1782, 1783,
-        1784, 1785, 1786, 1787, 1788, 1789, 1790, 1791, 1792, 1793, 1794,
-        1795, 1796, 1797, 1798, 1799, 1800, 1801, 1802, 1803, 1804, 1805,
-        1806, 1807, 1808, 1809, 1810, 1811, 1812, 1813, 1814, 1815, 1816,
-        1817, 1818, 1819, 1820, 1821, 1822, 1823, 1824, 1825, 1826, 1827,
-        1828, 1829, 1830, 1831, 1832, 1833, 1834, 1835, 1836, 1837, 1838,
-        1839, 1840, 1841, 1842, 1843, 1844, 1845, 1846, 1847, 1848, 1849,
-        1850, 1851, 1852, 1853, 1854, 1855, 1856, 1857, 1858, 1859, 1860,
-        1872, 1873, 1874, 1875, 1876, 1877, 1878, 1879, 1880, 1881, 1882,
-        1883, 1884, 1885, 1886, 1887, 1888, 1889, 1890, 1891, 1892, 1893,
-        1894, 1895, 1896, 1897, 1898, 1899, 1900, 1901, 1902, 1903, 1904,
-        1905, 1906, 1907, 1908, 1909, 1910, 1911, 1912, 1913, 1914, 1915,
-        1916, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926,
-        1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937,
-        1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948,
-        1949, 1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959,
-        1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970,
-        1971, 1972, 1973, 1974, 1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2023];
+    const yearList = [1960,1961,1962,1963,1964,1965,1966,1967,1968,1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019];
     const options = []
     for(let i=0; i<yearList.length; i++){
         options.push(<DropdownItem onClick={e=>this.changeYear(e,yearList[i], param, url)}><div>{yearList[i]}</div></DropdownItem>);
     }
     return options;
   }
+
 //   get_m_1_2(){
 //     fetch('http://127.0.0.1:5000/mockup_1_2', {
 //             method: 'POST',
@@ -433,7 +415,7 @@ class GlobalTemp extends React.Component {
             <container>
                 <Row className="justify-content-center">
                     <div align="center">
-                        <h2>Atmosphere Temperature VS Surface Temperature</h2>
+                        <h3>Atmosphere Temperature VS Surface Temperature</h3>
                     </div>
                 </Row>
                 <Row className="justify-content-center">
@@ -539,7 +521,7 @@ class GlobalTemp extends React.Component {
             <container>
                 <Row className="justify-content-center">
                     <div align="center">
-                        <h2>Something something something</h2>
+                        <h3>Number of Countries facing extreme weather events</h3>
                     </div>
                 </Row>
                 <Row>
@@ -552,6 +534,7 @@ class GlobalTemp extends React.Component {
                             </Col>
                         </Row>
                         <Row>
+                            <Col></Col>
                             <Col>
                                 <div align="center">
                                 <h6 className='text-black'>From</h6>
@@ -565,6 +548,7 @@ class GlobalTemp extends React.Component {
                                 </UncontrolledDropdown>
                                 </div>
                             </Col>
+                            <Col></Col>
                             <Col>
                                 <div align="center">
                                 <h6 className='text-black'>To</h6>
@@ -578,6 +562,7 @@ class GlobalTemp extends React.Component {
                                 </UncontrolledDropdown>
                                 </div>
                             </Col>
+                            <Col></Col>
                         </Row>
                     </Col>
                     <Col lg="7" className="align-self-center">
@@ -585,7 +570,11 @@ class GlobalTemp extends React.Component {
                     </Col>
                     <Col lg="2" className="align-self-center">
                         <div>
-                            <p>Something something something Something something something Something something something Something something something Something something something Something something something Something something something</p>
+                            <p><li>Formula : If the the percentage of temperature change between the consecutive months is
+                                more than 35% for atleast 4 months in a year.</li>
+
+                                <li>While there isnt a clear upward trend in the overall data,
+                                further drilled down inspection can reveal peaks in certain geograpghic areas.</li></p>
                         </div>
                     </Col>
                 </Row>
@@ -596,7 +585,7 @@ class GlobalTemp extends React.Component {
             <container>
                 <Row className="justify-content-center">
                     <div align="center">
-                        <h2>Something something something</h2>
+                        <h3>Atmospheric Temperature</h3>
                     </div>
                 </Row>
                 <Row className="justify-content-center">
@@ -657,7 +646,9 @@ class GlobalTemp extends React.Component {
                     </Col>
                     <Col lg="2" className="align-self-center mr-4">
                         <div>
-                            <p>Something something something Something something something Something something something Something something something Something something something Something something something Something something something</p>
+                            <p><li>The chart display average, minimum, maximum and standard deviation of temperatures within a year.</li>
+
+                            <li>Slight upward trand can be observed in the minimum temperatures.</li></p>
                         </div>
                     </Col>
                 </Row>
